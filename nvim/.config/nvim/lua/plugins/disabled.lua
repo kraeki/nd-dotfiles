d483bf629b2cd8ace80a5e1520c0208287284e3b
Andreas Schmid <ikeark@gmail.com>
2024-06-30 22:03:51 +0200

    init

---
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  --disable noice
+  { "folke/noice.nvim",       enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "folke/flash.nvim",       enabled = false }, -- disable search labels
+}
