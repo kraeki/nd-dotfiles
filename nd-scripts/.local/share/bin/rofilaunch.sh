1611738bdb9f62e1533d9a1668d10ad8977af53c
Andreas Schmid <ikeark@gmail.com>
2024-07-13 15:14:52 +0200

    Fix rofi to run in float

--- nd-scripts/.local/share/bin/rofilaunch.sh
+++ nd-scripts/.local/share/bin/rofilaunch.sh
@@ -19,25 +19,13 @@ d | --drun) r_mode="drun" ;;
 w | --window) r_mode="window" ;;
 f | --filebrowser) r_mode="filebrowser" ;;
 h | --help)
-	echo -e "$(basename "${0}") [action]"
-	echo "d :  drun mode"
-	echo "w :  window mode"
-	echo "f :  filebrowser mode,"
-	exit 0
-	;;
+  echo -e "$(basename "${0}") [action]"
+  echo "d :  drun mode"
+  echo "w :  window mode"
+  echo "f :  filebrowser mode,"
+  exit 0
+  ;;
 *) r_mode="drun" ;;
 esac
 
-#// set overrides
-
-#wind_border=$((hypr_border * 3))
-#[ "${hypr_border}" -eq 0 ] && elem_border="10" || elem_border=$((hypr_border * 2))
-#r_override="window {border: ${hypr_width}px; border-radius: ${wind_border}px;} element {border-radius: ${elem_border}px;}"
-#r_scale="configuration {font: \"JetBrainsMono Nerd Font ${rofiScale}\";}"
-#i_override="$(gsettings get org.gnome.desktop.interface icon-theme | sed "s/'//g")"
-#i_override="configuration {icon-theme: \"${i_override}\";}"
-
-#// launch rofi
-
-#rofi -show "${r_mode}" -theme-str "${r_scale}" -theme-str "${r_override}" -theme-str "${i_override}" -config "${roconf}"
 rofi -show "${r_mode}" -config "${roconf}"
