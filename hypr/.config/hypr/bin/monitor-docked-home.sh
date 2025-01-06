76bf0f041e38de4ffb9ef485bfb151ccb8472246
Andreas Schmid <ikeark@gmail.com>
2025-01-06 01:22:51 +0100

    Move stow nd-scripts package under hypr package

---
+++ hypr/.config/hypr/bin/monitor-docked-home.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+hyprctl dispatch exec "hyprctl keyword monitor eDP-1,disabled"
+hyprctl dispatch exec "hyprctl keyword monitor 'Dell Inc. DELL U2518D 3C4YP898AHPL',enabled,transform,90,position,0,0"
+hyprctl dispatch exec "hyprctl keyword monitor 'Dell Inc. DELL U2518D 3C4YP8A4AGBL',enabled,position,1440,560"
