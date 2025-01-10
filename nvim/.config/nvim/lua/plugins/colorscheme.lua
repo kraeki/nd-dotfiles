e8d215a2ba4a10ef8d0c088ea9ec640585b1c545
Andreas Schmid <ikeark@gmail.com>
2025-01-10 21:41:51 +0100

    Neovim: Use dracula color theme with patched bgcolor Overriding the original bgcolor to match the Dracula theme of ghostty

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
-  -- add gruvbox
-  { "catppuccin/nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    "dracula/vim",
+    name = "dracula",
+    lazy = false, -- Load immediately
+    priority = 1000, -- Ensure it loads before other plugins
+    config = function()
+      -- Apply the Dracula theme
+      vim.cmd.colorscheme("dracula")
+
+      -- Override the background color
+      vim.cmd("highlight Normal guibg=#1E1F29")
+    end,
   },
 }
