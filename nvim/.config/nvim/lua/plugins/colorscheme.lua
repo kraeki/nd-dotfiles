70a7103768ad454eaccaf7a2f74bbde15ac1a665
Andreas Schmid <ikeark@gmail.com>
2025-05-28 12:37:13 +0200

    nvim: Update colorscheme to catpuuccin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,16 @@
 return {
   {
-    "dracula/vim",
-    name = "dracula",
-    lazy = false, -- Load immediately
-    priority = 1000, -- Ensure it loads before other plugins
-    config = function()
-      -- Apply the Dracula theme
-      vim.cmd.colorscheme("dracula")
-
-      -- Override the background color
-      vim.cmd("highlight Normal guibg=#1E1F29")
-    end,
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
   },
 }
