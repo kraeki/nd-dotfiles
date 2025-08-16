02b927de973f35e0b521af4235d822ae13b2284b
Andreas Schmid <ikeark@gmail.com>
2025-08-16 02:09:06 +0200

    NixOS: Allow insecure package for ulauncher

--- nixos-config/hosts/naptop/configuration.nix
+++ nixos-config/hosts/naptop/configuration.nix
@@ -68,6 +68,10 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  # FIMXE: Due to Ulauncher 
+  nixpkgs.config.permittedInsecurePackages = [
+    "libsoup-2.74.3"
+  ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
